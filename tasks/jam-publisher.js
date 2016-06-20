bcd7a75d2a5d140060fcc9188d596f05f9af02f2
Artem Mezin <iketari@gmail.com>
2016-06-20 14:13:55 +0300

    0.2.0 do not publish without tag

--- tasks/jam-publisher.js
+++ tasks/jam-publisher.js
@@ -8,36 +8,69 @@
 
 'use strict';
 
-var path = require('path');
-var jam = require('jamjs');
+const path = require('path');
+const jam = require('jamjs');
+const git = require('git-promise');
+
+const EXISTS_RE = /^Entry already exists/;
 
 module.exports = function (grunt) {
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
+  function checkGitTag (tag, remote) {
+      const gitCommang = `git ls-remote ${remote} refs/tags/${tag}`;
+      grunt.log.debug(gitCommang);
+
+      return new Promise((resolve, reject) => {
+          git(gitCommang).then(data => {
+              if (!data) {
+                  let error = `Can't find git ref`;
+                  grunt.log.error(error);
+                  return reject(data);
+              }
+
+              resolve(data);
+          }, err => {
+              grunt.log.error('Something wrong with git');
+              grunt.log.error(err);
+              reject(err);
+          });
+      });
+  }
+
+  function publish (options) {
+      return new Promise ((resolve, reject) => {
+          jam.publish({
+            dir: path.resolve(options.dir || '.'),
+            repo: options.repo,
+            level: options.level || null
+          }, function (error) {
+
+            grunt.log.debug(error);
+
+            if (error && !EXISTS_RE.test(error)) {
+              grunt.log.error('Something wrong with publishing');
+            } else {
+              grunt.log.ok('Success publishing.' + (EXISTS_RE.test(error) ? ' Don\'t worry about error in terminal' : ''));
+            }
+          });
+      });
+  }
+
   grunt.registerTask('jam-publisher', 'Task for publishing jam packages', function () {
+    const options = this.options();
+    const done = this.async();
+    const packageCfg = options.cfg || {};
 
-    var options = this.options();
-    var done = this.async();
-    var EXISTS_RE = /^Entry already exists/
+    let remote = options.remote || 'origin',
+        tag = packageCfg.version;
 
-    jam.publish({
-      dir: path.resolve(options.dir || '.'),
-      repo: options.repo,
-      level: options.level || null
-    }, function (error) {
-
-      grunt.log.debug(error);
-
-      if (error && !EXISTS_RE.test(error)) {
-        grunt.log.error('Something wrong with publishing');
-      } else {
-        grunt.log.ok('Success publishing.' + (EXISTS_RE.test(error) ? ' Don\'t worry about error in terminal' : ''));
-      }
-
-      done();
-    });
+    checkGitTag(tag, remote)
+        .then(data => {
+            publish(options).then(done, err => grunt.console.error(err));
+        }, err => grunt.console.error(err));
 
   });
 };
